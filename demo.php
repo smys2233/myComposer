b12380b43050063fa3eb27d12823e5f5911feef5
gongzhi <smys2233@126.com>
2019-11-29 11:56:15 +0800

    我的第一个版本

---
+++ demo.php
@@ -0,0 +1,5 @@
+<?php
+require_once './vendor/autoload.php';
+use Gongzhi\Mycomposer\Tools;
+Tools::Hello();
+?>
---
+++ src/Gongzhi/Mycomposer/Tools.php
@@ -0,0 +1,12 @@
+<?php
+namespace Gongzhi\Mycomposer;
+
+class Tools{
+	public function __construct(){
+		
+	}
+	public static function Hello(){
+		echo "Hello World";
+	}
+}
+?>
